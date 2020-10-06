cff0ce4f4e60183fbb84b02fdb31c4973894387e
Vitor Braga <vitor.braga@tomtom.com>
2020-10-06 20:24:38 +0200

    removing dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
-const env = dotenv.config().parsed;
+// const env = dotenv.config().parsed;
 
-const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-}, {});
+// const envKeys = Object.keys(env).reduce((prev, next) => {
+//     prev[`process.env.${next}`] = JSON.stringify(env[next]);
+//     return prev;
+// }, {});
 
 module.exports = {
     context: __dirname,
